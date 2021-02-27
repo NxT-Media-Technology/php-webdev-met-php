2e40ae44306aef47e0f0edb7838a2437f0872ab8
Sam Serrien <sam@genx.be>
2021-02-27 16:02:59 +0100

    IC

---
+++ layout/header.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My consulting website</title>
+    <meta name="description" content="My company website bla bla bla"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="/resources/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/resources/css/main.css" rel="stylesheet">
+    <link href="/resources/css/responsive.css" rel="stylesheet">
+
+    <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+    <link rel="shortcut icon" href="/resources//resources/images/favicon.png" type="image/x-icon">
+    <link rel="icon" href="/resources//resources/images/favicon.png" type="image/x-icon">
+    <meta name="msapplication-TileImage" content="/resources//resources/images/favicon.png" />
+</head>
+
+<body>
+
+<div class="page-wrapper">
+
+    <!-- Main Header-->
+    <header class="main-header">
+
+		<!-- Header Top -->
+        <div class="header-top">
+            <div class="auto-container">
+                <div class="inner-container clearfix">
+					<!-- Top Left -->
+					<div class="top-left">
+						<!-- Info List -->
+						<ul class="info-list">
+							<li><a href="mailto:hello@consulte.co"><span class="icon icofont-envelope"></span> hello@consulte.co</a></li>
+							<li><a href="tel:+1212-226-3126"><span class="icon icofont-phone"></span> +1212-226-3126</a></li>
+							<li><a href="contact.html"><span class="icon icofont-clock-time"></span> Mon - Sat: 8.00 - 17.00, Sunday Closed</a></li>
+						</ul>
+					</div>
+
+					<!-- Top Right -->
+                    <div class="top-right pull-right">
+						<!-- Social Box -->
+						<ul class="social-box">
+							<li class="share">Our Social</li>
+							<li><a href="https://twitter.com/" class="icofont-twitter"></a></li>
+							<li><a href="http://facebook.com/" class="icofont-facebook"></a></li>
+							<li><a href="https://www.instagram.com/" class="icofont-instagram"></a></li>
+							<li><a href="https://rss.com/" class="icofont-rss"></a></li>
+							<li><a href="https://www.youtube.com/" class="icofont-play-alt-1"></a></li>
+						</ul>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    	<!-- Header Upper -->
+        <div class="header-upper">
+        	<div class="auto-container">
+            	<div class="inner-container clearfix">
+
+                	<div class="pull-left logo-box">
+                    	<div class="logo"><a href="index.html"><img src="/resources/images/logo.png" alt="" title=""></a></div>
+                    </div>
+
+                   	<div class="nav-outer pull-left clearfix">
+						<!-- Main Menu -->
+						<nav class="main-menu navbar-expand-md">
+							<div class="navbar-header">
+								<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+								</button>
+							</div>
+
+							<div class="navbar-collapse show collapse clearfix" id="navbarSupportedContent">
+								<ul class="navigation clearfix">
+									<li class="current"><a href="index.php">Home</a></li>
+									<li><a href="about.php">About</a></li>
+									<li class="dropdown"><a href="#">Service</a>
+										<ul>
+											<li><a href="service.php">Service</a></li>
+											<li><a href="service-detail.php">Service Detail</a></li>
+										</ul>
+									</li>
+									<li class="dropdown"><a href="#">Projects</a>
+										<ul>
+											<li><a href="project.php">Project</a></li>
+											<li><a href="project-details.php">Project Details</a></li>
+										</ul>
+									</li>
+									<li class="dropdown"><a href="#">Blog</a>
+										<ul class="from-right">
+											<li><a href="blog.php">Our Blog</a></li>
+											<li><a href="blog-detail.php">Blog Detail</a></li>
+										</ul>
+									</li>
+									<li><a href="contact.php">Contact</a></li>
+								</ul>
+							</div>
+
+						</nav>
+
+					</div>
+
+					<!-- Outer Box -->
+					<div class="outer-box">
+						<!-- Search Btn -->
+						<div class="search-box-btn search-box-outer"><span class="icon icofont-search"></span></div>
+						<!-- Mobile Navigation Toggler -->
+                        <div class="mobile-nav-toggler"><span class="icon ti-menu"></span></div>
+					</div>
+
+                </div>
+            </div>
+        </div>
+        <!--End Header Upper-->
+
+		<!-- Mobile Menu  -->
+        <div class="mobile-menu">
+            <div class="menu-backdrop"></div>
+            <div class="close-btn"><span class="icon lnr lnr-cross"></span></div>
+
+            <nav class="menu-box">
+                <div class="nav-logo"><a href="index.php"><img src="/resources/images/logo.png" alt="" title=""></a></div>
+                <div class="menu-outer"><!--Here Menu Will Come Automatically Via Javascript / Same Menu as in Header--></div>
+            </nav>
+        </div><!-- End Mobile Menu -->
+
+    </header>
+    <!--End Main Header -->
---
+++ project.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My consulting website</title>
+    <meta name="description" content="My company website bla bla bla"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="/resources/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/resources/css/main.css" rel="stylesheet">
+    <link href="/resources/css/responsive.css" rel="stylesheet">
+
+    <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+    <link rel="shortcut icon" href="/resources//resources/images/favicon.png" type="image/x-icon">
+    <link rel="icon" href="/resources//resources/images/favicon.png" type="image/x-icon">
+    <meta name="msapplication-TileImage" content="/resources//resources/images/favicon.png" />
+</head>
+
+<body>
+
+<div class="page-wrapper">
+
+	<!-- Page Title Section -->
+    <div class="page-title-section">
+    	<div class="auto-container">
+			<ul class="post-meta">
+				<li><a href="index.html">Index</a></li>
+				<li>Project</li>
+			</ul>
+			<h2><span>Latest</span> From Our Project</h2>
+		</div>
+	</div>
+	<!-- End Page Title Section -->
+
+	<!-- Project Section -->
+	<div class="project-section section-padding">
+		<div class="outer-container">
+			<div class="row clearfix">
+
+				<!-- Column -->
+				<div class="column col-lg-6 col-md-12 col-sm-12">
+					<div class="row clearfix">
+
+						<!-- Inner Column -->
+						<div class="inner-column col-lg-6 col-md-6 col-sm-12">
+							<div class="gallery-block">
+								<div class="inner-box">
+									<div class="image">
+										<img src="/resources/images/gallery/2.jpg" alt="" />
+										<div class="overlay-box">
+											<div class="overlay-inner">
+												<h3><a href="project-details.html">WOTa Money Save</a></h3>
+												<div class="designation">Finance, Consulting</div>
+												<a href="project-details.html" class="arrow ti-angle-right"></a>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<!-- Inner Column -->
+						<div class="inner-column col-lg-6 col-md-6 col-sm-12">
+							<div class="gallery-block">
+								<div class="inner-box">
+									<div class="image">
+										<img src="/resources/images/gallery/3.jpg" alt="" />
+										<div class="overlay-box">
+											<div class="overlay-inner">
+												<h3><a href="project-details.html">WOTa Money Save</a></h3>
+												<div class="designation">Finance, Consulting</div>
+												<a href="project-details.html" class="arrow ti-angle-right"></a>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<!-- Inner Column -->
+						<div class="inner-column col-lg-12 col-md-12 col-sm-12">
+							<div class="gallery-block">
+								<div class="inner-box">
+									<div class="image">
+										<img src="/resources/images/gallery/4.jpg" alt="" />
+										<div class="overlay-box">
+											<div class="overlay-inner">
+												<h3><a href="project-details.html">WOTa Money Save</a></h3>
+												<div class="designation">Finance, Consulting</div>
+												<a href="project-details.html" class="arrow ti-angle-right"></a>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+					</div>
+				</div>
+
+				<!-- Column -->
+				<div class="column col-lg-6 col-md-12 col-sm-12">
+					<div class="gallery-block">
+						<div class="inner-box">
+							<div class="image">
+								<img src="/resources/images/gallery/1.jpg" alt="" />
+								<div class="overlay-box">
+									<div class="overlay-inner">
+										<h3><a href="project-details.html">WOTa Money Save</a></h3>
+										<div class="designation">Finance, Consulting</div>
+										<a href="project-details.html" class="arrow ti-angle-right"></a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+	<!-- End Project Section -->
+
+<?php include('layout/footer.php'); ?>
